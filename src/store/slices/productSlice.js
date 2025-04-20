c016b1abdfca33d1055edc2865c7477c5ee2a465
Himanshu <himanshudubeuhd@gmail.com>
2025-04-20 14:18:34 +0530

    Resolved merge conflicts: accepted local versions

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import { createSlice } from '@reduxjs/toolkit';
 
@@ -14,20 +13,3 @@ const productSlice = createSlice({
 
 export const { setProducts } = productSlice.actions;
 export default productSlice.reducer;
-=======
-
-import { createSlice } from '@reduxjs/toolkit';
-
-const productSlice = createSlice({
-  name: 'products',
-  initialState: [],
-  reducers: {
-    setProducts(state, action) {
-      return action.payload;
-    },
-  },
-});
-
-export const { setProducts } = productSlice.actions;
-export default productSlice.reducer;
->>>>>>> 3ee5ce2c11e67ef438a0f1bf6266aa913f7b6820
